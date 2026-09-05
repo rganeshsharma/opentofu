fc391c90c2fd55672e9a99d01947dfbd35f8dd41
Ganesh Sharma <44496498+rganeshsharma@users.noreply.github.com>
2026-09-05 14:22:10 +0530

    Create gpu-node.tf

---
+++ digital-ocean/digital-ocean-cloud-infra/infra/platform/gpu-node.tf
@@ -0,0 +1,17 @@
+resource "digitalocean_droplet" "gpu_node" {
+
+  name   = "${var.project_name}-gpu-node"
+  region = var.region
+  image  = var.gpu_image
+  size   = var.gpu_size
+  vpc_uuid = digitalocean_vpc.lab.id
+  ssh_keys = [
+    digitalocean_ssh_key.ai_lab.fingerprint
+  ]
+  tags = [
+    digitalocean_tag.rke2_gpu.name
+  ]
+  monitoring = false
+  backups = false
+  graceful_shutdown = true
+}
